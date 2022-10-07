cfabb0b8416851658fef6da95fa01a497fba62d9
mustafa6625 <mustafasiddiqui60@gmail.com>
2022-10-07 16:18:48 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '72f3dd53-c074-436a-b4b8-8bd66fec6791';
+export const MODAL_CLOSE_SEC = 2.5;
